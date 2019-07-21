6fb34683440ceb72a1c98b897884eb0318f02dc6
Ayyoub Al-Qaissi <Ayyoub.Al-Qaissi@Globitel.com>
2019-07-21 16:38:42 +0300

    packaging

--- Core Components/template/Builder.bat
+++ Core Components/template/Builder.bat
@@ -1,4 +1,12 @@
 SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_66
+
+call cd ../log4j-globitel_v2
+call Builder.bat
+
+xcopy "BuildData\log4j-globitel-v2.0.0.jar" "..\template\lib\log4j-common"
+
+call cd ../template
+
 call ant
 
 RD bin /S/Q
